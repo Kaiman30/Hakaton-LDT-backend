4f26a7a53302cb5ac5a70617f81ebf2217e18213
yume <kaiman4ik@internet.ru>
2026-08-24 21:29:35 +0300

    api gateway on work

--- API-Gateway/cmd/api-gateway/main.go
+++ API-Gateway/cmd/api-gateway/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"Hakaton-LDT.API-Gateway/internal/api"
+	"Hakaton-LDT.API-Gateway/internal/config"
+)
 
 func main() {
-	fmt.Println("Я запущен")
+	// Загрузка конфига
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	router, err := api.NewRouter(cfg)
+	if err != nil {
+		log.Fatalf("init router: %v", err)
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", cfg.HTTP.Port),
+		Handler: router,
+	}
+
+	go func() {
+		log.Printf("gateway listening on port %s", cfg.HTTP.Port)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+	<-ctx.Done()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("server shutdown: %v", err)
+	}
+
+	log.Println("gateway shutdown complete")
 }
---
+++ API-Gateway/go.mod
@@ -0,0 +1,8 @@
+module Hakaton-LDT.API-Gateway
+
+go 1.26.6
+
+require (
+	github.com/go-chi/chi/v5 v5.3.2 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+)
