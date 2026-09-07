78fe66c64041ce89600a95bc2bce8986428a8e43
yume <kaiman4ik@internet.ru>
2026-09-07 19:41:09 +0300

    add fields to Config struct and AuthConfig struct

--- API-Gateway/internal/config/config.go
+++ API-Gateway/internal/config/config.go
@@ -3,11 +3,18 @@ package config
 import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	HTTP HTTPConfig
+	HTTP      HTTPConfig
+	JWTSecret string `envconfig:"JWT_SECRET" default:"dev-secret"`
+	Auth      AuthConfig
 }
 
 type HTTPConfig struct {
 	Port string `envconfig:"HTTP_PORT" default:"8000"`
+}
+
+type AuthConfig struct {
+	Host string `envconfig:"HOST" default:"localhost"`
+	Port string `envconfig:"PORT" default:"8001"`
 }
 
 func Load() (*Config, error) {
